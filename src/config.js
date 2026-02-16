031350d124982be7e8c2213e9af757e1ec829475
sudarshan udawat <itzudawat@gmail.com>
2026-02-16 14:09:47 +0530

    Add Server Backend Url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Change this to your backend URL (use your computer's IP for physical device testing)
-export const API_URL = "http://172.20.10.2:3001/api";
+export const API_URL = "https://expense-tracker-app-14hd.onrender.com/api";
 // For Android emulator use: http://10.0.2.2:3001/api
 // For physical device use: http://192.168.137.1:3001/api (replace with your actual IP)
 // Alternative IPs (if above doesn't work): 
